2a8a80a24d45f1b4cb0c669fc42fa400ab5efb37
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-06-06 23:33:10 +0700

    feature register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,14 @@
+
 const { registerService } = require("../services/auth.service")
 
 const register = async (req, res) => {
   const newUser = await registerService(req.body);
+  console.log(newUser)
+  return res.status(201).json({
+    status: 'success',
+    message: 'Register successfully',
+    data: newUser
+  })
 }
 
 module.exports = {
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,11 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
-const registerService = (body) => {
-   
+const registerService = async (body) => {
+  const { username, password } = body;
+  const hashed = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ username: username, password: hashed });
+  return newUser;
 }
 
 module.exports = {

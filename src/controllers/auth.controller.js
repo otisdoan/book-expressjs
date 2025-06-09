3f1cd16dc43e6c513f1ec389b26a81364b4e3cba
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-06-09 09:57:45 +0700

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,8 @@
 const { registerService, loginService, refreshTokenService } = require("../services/auth.service")
 
 const register = async (req, res) => {
-  const newUser = await registerService(req.body);
+  const { username, password } = req.body;
+  const newUser = await registerService(username, password);
   console.log(newUser)
   return res.status(201).json({
     status: 'success',
@@ -12,20 +13,36 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-  const account = await loginService(req.body);
-  return res.status(200).json({
-    status: 'success',
-    message: 'Login successfully',
-    data: account
-  })
+  try {
+    const { username, password } = req.body;
+    const account = await loginService(username, password);
+    return res.status(200).json({
+      status: 'success',
+      message: 'Login successfully',
+      data: account
+    })
+  } catch (error) {
+    res.status(401).json({
+      status: 'fail',
+      message: error.message
+    })
+  }
 }
 
-const refreshToken = (req, res) => {
-  const results = refreshTokenService(req.body);
-  return res.status(200).json({
-    status: 'success',
-    data: results
-  })
+const refreshToken = async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
+    const results = await refreshTokenService(refreshToken);
+    return res.status(200).json({
+      status: 'success',
+      data: results
+    })
+  } catch (error) {
+    res.status(401).json({
+      status: 'fail',
+      message: error.message
+    })
+  }
 }
 
 module.exports = {
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,46 +4,54 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const registerService = async (body) => {
-  const { username, password } = body;
+const registerService = async (username, password) => {
   const hashed = await bcrypt.hash(password, 10);
   const newUser = await User.create({ username: username, password: hashed });
   return newUser;
 }
 
-const loginService = async (body) => {
-  const { username, password } = body;
+const loginService = async (username, password) => {
+
   const user = await User.findOne({ username: username });
-  if (!user) return res.status(404).json('Username is not found!');
+
+  if (!user) throw new Error('User not found!');
+
   const passwordValid = await bcrypt.compare(password, user.password);
-  if (!passwordValid) return res.status(401).json('Password wrong!');
+
+  if (!passwordValid) throw new Error('Password is invalid!');
+
   const accessToken = jwt.sign(
     { _id: user._id, username: user.username, role: user.role },
     process.env.JWT_ACCESS_TOKEN,
     { expiresIn: '1m' }
   );
+
   const refreshToken = jwt.sign(
     { _id: user._id, username: user.username },
     process.env.JWT_REFRESH_TOKEN,
     { expiresIn: '7d' }
   )
-  try {
-    await Token.create({ refreshToken: refreshToken });
-  } catch (error) {
-    console.log(error);
-  }
 
-  return { user, accessToken, refreshToken }
+  await Token.create({ refreshToken: refreshToken });
+
+  return { user, accessToken, refreshToken };
+
 }
 
-const refreshTokenService = async (body) => {
-  // const { refreshToken } = body;
-  // const user = await jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN);
-  // const refreshTokenOld = await Token.findOne({ refreshToken: refreshToken });
-  // if (!refreshTokenOld) return res.status(401).json({ message: 'Token is invalid' })
-  // const accessTokenNew = await jwt.sign({ _id: user._id, username: user.username, role: user.role }, process.env.JWT_ACCESS_TOKEN, { expiresIn: '1m' });
-  // const refreshTokenNew = await jwt.sign({ _id: user._id, username: user.username, role: user.role }, process.env.JWT_REFRESH_TOKEN, { expiresIn: '7d' });
-  // return { accessTokenNew, refreshTokenNew };
+const refreshTokenService = async (refreshToken) => {
+
+  const user = jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN);
+
+  const refreshTokenValid = await Token.findOne({ refreshToken: refreshToken });
+
+  if (!refreshTokenValid) throw new Error('Token not found!');
+
+  const accessTokenNew = jwt.sign({ _id: user._id, username: user.username, role: user.role }, process.env.JWT_ACCESS_TOKEN, { expiresIn: '1m' });
+
+  const refreshTokenNew = jwt.sign({ _id: user._id, username: user.username, role: user.role }, process.env.JWT_REFRESH_TOKEN, { expiresIn: '7d' });
+
+  return { accessToken: accessTokenNew, refreshToken: refreshTokenNew };
+
 }
 
 module.exports = {
